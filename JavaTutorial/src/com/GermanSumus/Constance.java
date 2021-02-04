841512ad643cf531d487bb6e336f9ac0cedbb40e
German Rodriguez <GermanRodriguezBusiness@gmail.com>
2021-02-04 12:57:44 -0600

    simple constance examples

---
+++ JavaTutorial/src/com/GermanSumus/Constance.java
@@ -0,0 +1,15 @@
+package com.GermanSumus;
+
+public class Constance {
+    public static void main(String[] args) {
+        // The key word final prevents the variable to change later on
+        final float pi = 3.14F;
+        final int birthYear = 1996;
+        final short maxNumberOfLoans = 3;
+        System.out.println(pi);
+        System.out.println(birthYear);
+        System.out.println(maxNumberOfLoans);
+
+
+    }
+}
